4ed309de5f29ab305fc8f7a7aa51772ca83aa49d
CHASE Eve <eachase@email.wm.edu>
2016-08-08 12:06:12 -0500

    Include full prior range for x axis

--- scripts/make2Dcdfs.py
+++ scripts/make2Dcdfs.py
@@ -3,7 +3,5 @@ import subprocess
 
 parameters = [x for x in itertools.combinations(['mc', 'q', 'a1', 'a2', 'tilt1', 'tilt2'], 2)]
 for pair in parameters:
-	param1 = pair[0]
-	param2 = pair[1]
-	subprocess.call(["python", "2dcdfs.py", "--param1", param1, "--param2", param2, "--basepath", "/projects/b1011/spinning_runs/freezingparams_20160402", "--errors", "bounded", "--confidence", "90"])
+	subprocess.call(["python", "2dcdfs.py", "--param1", pair[0], "--param2", pair[1], "--basepath", "/projects/b1011/spinning_runs/freezingparams_20160402", "--errors", "bounded", "--confidence", "90"])
 
