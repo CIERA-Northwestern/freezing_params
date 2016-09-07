2aad22cf907f24249a33d002b2eb7a8884c50ad4
Chris Pankow <chris.pankow@ligo.org>
2016-09-07 13:21:18 -0500

    make2Dcdfs.py: add bpath argument

--- scripts/make2Dcdfs.py
+++ scripts/make2Dcdfs.py
@@ -1,7 +1,10 @@
+import sys
 import itertools
 import subprocess
 
+bpath = sys.argv[1]
+
 parameters = [x for x in itertools.combinations(['mc', 'q', 'a1', 'a2', 'tilt1', 'tilt2'], 2)]
 for pair in parameters:
-	subprocess.call(["python", "2dcdfs.py", "--param1", pair[0], "--param2", pair[1], "--basepath", "/projects/b1011/spinning_runs/freezingparams_20160402", "--errors", "bounded", "--confidence", "90"])
+	subprocess.call(["python", "2dcdfs.py", "--param1", pair[0], "--param2", pair[1], "--basepath", bpath, "--errors", "bounded", "--confidence", "90"])
 
